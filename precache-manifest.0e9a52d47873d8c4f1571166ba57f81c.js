2eda4650867e831890b296e05c908cb7f4727b45
Thong Nguyen <tmn9uy3n@gmail.com>
2019-10-24 23:47:39 +1030

    Updates

---
+++ precache-manifest.0e9a52d47873d8c4f1571166ba57f81c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b1d60909430c99166aa3e80cd01ffa",
+    "url": "/crud-react-hooks/index.html"
+  },
+  {
+    "revision": "d8f772a3a26c731ed9dc",
+    "url": "/crud-react-hooks/static/css/main.b7bfb6b0.chunk.css"
+  },
+  {
+    "revision": "4a360a71a371b3588af2",
+    "url": "/crud-react-hooks/static/js/2.e6f4febd.chunk.js"
+  },
+  {
+    "revision": "d8f772a3a26c731ed9dc",
+    "url": "/crud-react-hooks/static/js/main.ed52fc45.chunk.js"
+  },
+  {
+    "revision": "1f23f302c676bd5e96b1",
+    "url": "/crud-react-hooks/static/js/runtime-main.54c67b7a.js"
+  }
+]);
